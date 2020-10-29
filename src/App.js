9195c5c601e90db8bfb1dc4b3ffecab45b224230
enmalafeev <malafeev.en@yandex.ru>
2020-10-29 15:05:14 +0300

    add example.ts

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import cn from 'classnames';
 
 import s from './App.modules.scss';
 import './custom.css';
+import { concat } from './example.ts';
 
 const App = () => {
   return (
+    <>
     <div className={cn(s.header, 'green')}>
       This is work! This is App Component!
     </div>
+  <p>{concat('Hello ', 'world!')}</p>
+    </>
   )
 };
 
