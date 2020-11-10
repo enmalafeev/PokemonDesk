962009c1a6b5a3a34594718007fd7f4458c6e132
enmalafeev <malafeev.en@yandex.ru>
2020-11-10 23:41:18 +0300

    add PageNotFound

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { navigate } from 'hookrouter';
+import s from './NotFoundPage.module.scss';
+import teamRocket from './assets/teamRocket.png';
+import Button from '../../components/Button';
 
 const NotFoundPage: React.FC = () => {
-  return <div>404 Not Found</div>;
+  return (
+    <div className={s.root}>
+      <div className={s.wrap}>
+        <div className={s.text}>404</div>
+        <div className={s.layer}>
+          <img src={teamRocket} alt="teamRocket" />
+          <div className={s.subtitle}>
+            <span>The rocket team</span> has won this time
+          </div>
+          <Button onClick={() => navigate('/')}>Return</Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NotFoundPage;
