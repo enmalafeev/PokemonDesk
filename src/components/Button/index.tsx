0f0d6ad9a386536c32a7d03948fb4f124d04fe21
enmalafeev <malafeev.en@yandex.ru>
2020-11-11 23:07:35 +0300

    add LinkEnum for routes

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import s from './Button.module.scss';
 
 interface ButtonProps {
-  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
   className?: string;
   width?: number;
   color?: string;
@@ -10,7 +10,7 @@ interface ButtonProps {
 }
 
 const Button: React.FC<ButtonProps> = ({ children, onClick, className, width, color, size }) => (
-  <button type="button" className={s.root}>
+  <button type="button" className={s.root} onClick={onClick}>
     {children}
   </button>
 );
