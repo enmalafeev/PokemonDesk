6a7a38af17f739c733a12ce2ea6c003b192cf542
enmalafeev <malafeev.en@yandex.ru>
2020-12-04 14:58:16 +0300

    add server render with handlebars

--- src/server/server.js
+++ src/server/server.js
@@ -1,4 +1,7 @@
 /* eslint-disable */
+import fs from 'fs';
+import path from 'path';
+import handlebars from 'handlebars';
 import Hapi from '@hapi/hapi';
 import React from 'react';
 import ReactDOM from 'react-dom/server';
@@ -11,13 +14,26 @@ const init = async () => {
     host: 'localhost',
   });
 
+  await server.register(require('@hapi/inert'));
+
+  server.route({
+    method: 'GET',
+    path: '/main.js',
+    handler: (request, h) => h.file(path.join(process.cwd(), 'dist', 'main.js')),
+  });
+
   server.route({
     method: 'GET',
     path: '/{any*}',
     handler: (request) => {
+      const pathIndexHTML = path.join(process.cwd(), 'dist', 'index.html');
+      const template = handlebars.compile(fs.readFileSync(pathIndexHTML, 'utf8'));
       setPath(request.path);
       const result = ReactDOM.renderToString(<App />);
-      return result;
+      const page = template({
+        content: result,
+      });
+      return page;
     },
   });
 
